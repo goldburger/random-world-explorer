0a6a5e92efebdba6e5f39dd1549e7c453a7daecb
Zoe Xi <contact@zoexi.com>
2015-05-28 14:07:03 -0700

    Refactor a bit. Add newest ver. of glMatrix

---
+++ texture.js
@@ -0,0 +1,22 @@
+
+var mudTexture;
+
+function initTexture() {
+    mudTexture = gl.createTexture();
+    mudTexture.image = new Image();
+    mudTexture.image.onload = function () {
+        handleLoadedTexture(mudTexture)
+    };
+
+    mudTexture.image.src = "mud.jpg";
+}
+
+function handleLoadedTexture(texture) {
+    gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true);
+    gl.bindTexture(gl.TEXTURE_2D, texture);
+    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, texture.image);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+
+    gl.bindTexture(gl.TEXTURE_2D, null);
+}
